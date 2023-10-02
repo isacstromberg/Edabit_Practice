8143397e0c30aeb51f2d92229f2886f398d2fcc4
Isac Strömberg <isacstromberg@hotmail.com>
2023-10-02 20:21:39 +0200

    #medium challenge complete

---
+++ GetTheIndexOfAWord/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GetTheIndexOfAWord
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string GetIndexOfAWord(string text)
+        {
+            List<string> listOfWords = new List<string> { "träd", "buske", "gren", "blad" };
+            int position = listOfWords.IndexOf(text);
+            return position.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Ordet ligger på plats {GetIndexOfAWord("blad")} i arrayen!");
+        }
+    }
+}
